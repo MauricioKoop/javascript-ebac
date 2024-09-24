7ce2042fe1bc12c143739de1581ca1e121abd1a5
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-09-24 20:34:30 -0300

    Estiliza elemento através do DOM.

---
+++ estiliza.js
@@ -0,0 +1,11 @@
+function estilizaCampo(){
+    let elTitulo = document.getElementById('estilizaTitulo');
+
+    elTitulo.style.backgroundColor = "orange";
+    elTitulo.style.fontSize = "40px";
+    elTitulo.style.textAlign = "center";
+    elTitulo.style.borderRadius = "50px";
+    elTitulo.style.padding = "20px 100px";
+    elTitulo.style.color = "white";
+    elTitulo.style.fontFamily = "Roboto";
+}
